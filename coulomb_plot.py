103a7466d78b4bcfc54e82443f03f539bef81774
Sasha Perigo <sasha.perigo@gmail.com>
2016-06-06 03:10:09 -0700

    Removed test file

--- coulomb_plot.py
+++
@@ -1,7 +0,0 @@
-from environment import Environment
-
-environ = Environment()
-environ.add_electron(0, 0, 0, 0)
-environ.add_electron(0, 1, 0, 0)
-environ.add_electron(1, 0, 0, 0)
-environ.plot_charges(1)
